6aec9ecac18211ffd26b13e2dd6646e686ea1736
gabrielheiwa <heiwa.gabriel@gmail.com>
2022-01-16 18:59:22 -0300

    ADD: delete repository button and request

--- components/repoCard/index.tsx
+++ components/repoCard/index.tsx
@@ -1,18 +1,52 @@
 import React from "react";
+import Axios from 'axios';
+
+import { toast } from 'react-toastify';
+import { TrashIcon } from '@heroicons/react/solid';
 
 interface RepoCardProps {
     reponame: string;
     id: number;
     url: string;
+    owner: string;
 }
 
-const RepoCard: React.FC<RepoCardProps> = ({ id, reponame, url }) => {
-    return <div className="flex flex-row p-4 my-2 mx-8 bg-gray-50 cursor-pointer">
-        <p className="w-44 truncate">{reponame}</p>
-        {/* <p className="">{id}</p> */}
-        <a href={url} target={"_blank"}>
-            <p>URL</p>
-        </a>
+const RepoCard: React.FC<RepoCardProps> = ({ id, reponame, url, owner }) => {
+
+    const handleClickDeleteRepository = async () => {
+        try {
+            const request = await Axios.delete(`https://api.github.com/repos/${owner}/${reponame}`, {
+                headers: {
+                    'Authorization': `token ${process.env.GITHUB_TOKEN}`,
+                    'Accept': 'application/vnd.github.v3+json'
+                }
+            });
+            
+            if (request.status === 204) {
+                toast.success('Repositório deletado com sucesso!');
+            };
+            
+        } catch (err) {
+            toast.error('Erro ao deletar o repositório!');
+        };
+    }
+
+    return <div className="flex flex-row justify-between p-4 my-2 mx-8 bg-white rounded-md">
+        <div className="flex flex-row items-center ">
+            <p className="w-44 text-gray-600 font-semibold truncate">{reponame}</p>
+            {/* <p className="">{id}</p> */}
+            <a href={url} target={"_blank"}>
+                <p className="text-gray-900 font-bold mx-4">URL</p>
+            </a>
+        </div>
+
+        <button 
+            onClick={handleClickDeleteRepository}
+            className="rounded-md p-2 text-gray-400 bg-gray-600 hover:bg-gray-400 hover:text-gray-600 transition delay-75">
+            <TrashIcon className="h-6 w-6" />
+        </button>
+
+
     </div>
 };
 
--- pages/ReposList/index.tsx
+++ pages/ReposList/index.tsx
@@ -8,6 +8,9 @@ interface repositorieProps {
     name: string;
     id: number;
     html_url: string;
+    owner: {
+        login: string;
+    }
 };
 
 
@@ -34,6 +37,7 @@ const ReposList: React.FC = () => {
         <div id="page-repo-list" className="w-screen h-screen overflow-auto bg-gray-600">
             { repositories.map((item, index) => (
                 <RepoCard key={index} 
+                    owner={item.owner.login}
                     reponame={item.name} 
                     id={item.id} 
                     url={item.html_url}/>
